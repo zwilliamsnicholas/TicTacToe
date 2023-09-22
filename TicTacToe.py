ba5f96ba3ef3e5e90e17f9177643fb1a056ecc0b
zwilliamsnicholas <115517054+zwilliamsnicholas@users.noreply.github.com>
2023-09-22 07:07:47 -0500

    Update TicTacToe.py
    added condition for when board is full

--- TicTacToe.py
+++ TicTacToe.py
@@ -8,7 +8,16 @@ def printboard():
     print(board[3] + "|" + board[4] + "|" + board[5])
     print(board[6] + "|" + board[7] + "|" + board[8])
     
-player = 1;
+sboard = ["1","2","3",
+         "4","5","6",
+         "7","8","9",]
+
+def printsboard():
+    #function prints the tic tac toe board
+    print(sboard[0] + "|" + sboard[1] + "|" + sboard[2])
+    print(sboard[3] + "|" + sboard[4] + "|" + sboard[5])
+    print(sboard[6] + "|" + sboard[7] + "|" + sboard[8])    
+
 def updateboard(player):
     move = int(input("Enter the number of the space on the board you'd like to move to."))
     move = move - 1
@@ -21,9 +30,11 @@ def updateboard(player):
             board[move] = "X";
     printboard()
 
-print("Welcome to Tic Tac Toe! Player 1 uses O, Player 2 uses X.")
-endcounter = 0; #play until wincounter equals 1 
+print("Welcome to Tic Tac Toe! Player 1 uses O, Player 2 uses X. Board spaces are numbered across.")
+printsboard();
+endcounter = 0; #play until endcounter equals 1 
 player =1; #switches betwen player 1 and 2, maybe division remainder of turnct? 
+turnct =0;
 while endcounter != 1:
     #player 1 plays 
     updateboard(player); 
@@ -31,8 +42,11 @@ while endcounter != 1:
     if board[0] == board[1] == board[2]=="O" or board[3] == board[4] == board[5]=="O" or board[6] == board[7] == board[8]=="O" or board[0] == board[3] == board[6]=="O" or board[1] == board[4] == board[7]=="O" or board[2] == board[5] == board[8]=="O" or board[0] == board[4] == board[8]=="O" or board[2] == board[4] == board[6]=="O" or board[0] == board[1] == board[2]=="X" or board[3] == board[4] == board[5]=="X" or board[6] == board[7] == board[8]=="X" or board[0] == board[3] == board[6]=="X" or board[1] == board[4] == board[7]=="X" or board[2] == board[5] == board[8]=="X" or board[0] == board[4] == board[8]=="X" or board[2] == board[4] == board[6]=="X":
         print("Player", player, " wins!"); 
         endcounter = 1; 
-    #elif #board is full and no more moves can be made 
+    elif board[0]!= '-' and board[1]!= '-' and board[2]!= '-' and board[3]!= '-' and board[4]!= '-' and board[5]!= '-' and board[6]!= '-' and board[7]!= '-' and board[8]!= '-':
+        print("Board full. Draw!"); 
+        endcounter = 1;
     else: #change player 
         turnct = turnct+1; 
         player = turnct%2 + 1; 
-        print(player);
+        print("Player", player, "'s turn!");
+        
